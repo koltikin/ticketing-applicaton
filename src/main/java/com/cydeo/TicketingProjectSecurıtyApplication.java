33b4a8519c5b460ea6f0fcc25960a352de366ef5
ziya <koltikin210@gmail.com>
2023-10-10 12:53:29 +0300

    SecurityFilterChain added

--- src/main/java/com/cydeo/TicketingProjectSecurıtyApplication.java
+++ src/main/java/com/cydeo/TicketingProjectSecurıtyApplication.java
@@ -2,20 +2,18 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
-public class TicketingProjectMvcApplication {
-
+public class TicketingProjectSecurıtyApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TicketingProjectMvcApplication.class, args);
+        SpringApplication.run(TicketingProjectSecurıtyApplication.class, args);
     }
 
 //    @Bean
 //    public MigrateResult migrateResult(DataSource dataSource){
 //        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
 //    }
-
 }
