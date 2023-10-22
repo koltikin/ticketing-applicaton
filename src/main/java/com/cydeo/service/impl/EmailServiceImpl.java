326f586a870158bff02d3827ade9bf5759633736
ziya <koltikin210@gmail.com>
2023-10-22 22:40:44 +0300

    text message successfully send

--- src/main/java/com/cydeo/service/impl/EmailServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/EmailServiceImpl.java
@@ -2,6 +2,7 @@ package com.cydeo.service.impl;
 
 import com.cydeo.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,13 +11,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class EmailServiceImpl implements EmailService {
-
+    private final Environment environment;
     private final JavaMailSender mailSender;
     @Override
     public void sendEmail(String sendTo, String subject, String message) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
 
-        mailMessage.setFrom("");
+        mailMessage.setFrom(environment.getProperty("EMAIL"));
         mailMessage.setTo(sendTo);
         mailMessage.setSubject(subject);
         mailMessage.setText(message);
