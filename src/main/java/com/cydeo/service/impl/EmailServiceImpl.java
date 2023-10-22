fab308c300981d8bf6e7e3288f3514e8f0513b6a
ziya <koltikin210@gmail.com>
2023-10-22 21:31:38 +0300

    EmailService is created

--- src/main/java/com/cydeo/service/impl/EmailServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/EmailServiceImpl.java
@@ -1,16 +1,11 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.service.EmailService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @RequiredArgsConstructor
 @Service
