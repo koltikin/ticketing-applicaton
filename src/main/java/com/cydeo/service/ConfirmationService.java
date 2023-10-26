07282b1ddf2f044188cd29d1c687dcb381ba5eba
ziya <koltikin210@gmail.com>
2023-10-26 23:04:08 +0300

    remove the old confirmation

---
+++ src/main/java/com/cydeo/service/ConfirmationService.java
@@ -0,0 +1,15 @@
+//package com.cydeo.service;
+//
+//import com.cydeo.entity.AccountConfirmation;
+//import com.cydeo.entity.User;
+//
+//public interface ConfirmationService {
+//    void saveConfirmation(String userName);
+//    String getTokenByUser(String userName);
+//    AccountConfirmation findByToken(String token);
+//
+//    Boolean isTokenExist(String token);
+//    void save(AccountConfirmation confirmation);
+//
+//    Boolean verifyAccount(String token);
+//}
